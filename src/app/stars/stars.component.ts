dc7dc827c356fe59577825693c8e7226e660bca4
pipeng119 <15640117732>
2018-11-08 16:30:23 +0800

    finished the second lesson

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.scss']
 })
 export class StarsComponent implements OnInit {
-
+  @Input() rating: number = 0;//input装饰器的含义就是rating这个变量应该有父组件传递进来
+  // private rating: number = 0;
+  private stars: boolean[] = [];
   constructor() { }
 
   ngOnInit() {
+    for(let i = 0; i < 5;i ++){
+      this.stars.push( i > this.rating);
+    }
   }
 
 }
